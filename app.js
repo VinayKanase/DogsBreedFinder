e6572402d9c18b8844627daf593432801c3c64c7
Vinay <vinaykanse9451@gmail.com>
2021-02-18 13:32:52 +0530

    Completed Home page started Images page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ function init(){
   "x-api-key" : "f6df9d10-8218-4fe1-9a61-d977d13c99c6"
  }
  // https://api.thedogapi.com/v1/breeds
+//  https://api.thedogapi.com/v1/images/search
  // https://api.thedogapi.com/v1/breeds/search?q=searchName
  fetch("https://api.TheDogAPI.com/v1/images/search?breed_id=10",sethead)
  .then(response=>response.json())
